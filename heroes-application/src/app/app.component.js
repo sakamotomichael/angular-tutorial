cb9e153666a688b234252fcdd6c8755a9b1d5f8f
Michael Sakamoto <ca07101210@celestica.com>
2017-10-11 11:09:40 -0400

    Post Master/Detail Page

--- heroes-application/src/app/app.component.js
+++ heroes-application/src/app/app.component.js
@@ -8,15 +8,36 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        this.title = 'Tip The Scales';
+        this.characters = Characters;
     }
+    AppComponent.prototype.onSelect = function (character) {
+        this.selectedCharacter = character;
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        template: "\n  <h1>Welcome to {{ title }}!</h1>\n  <h2>Gauntlet Character List</h2>\n  <ul class=\"characters\">\n    <li *ngFor=\"let character of characters\" \n      (click)=\"onSelect(character)\" \n      [class.selected]=\"character === selectedCharacter\" >\n    <span class=\"badge\"><b>{{character.id}}</b></span> {{character.name}}\n        <label><b>(Bracket: </b></label>\n        {{character.bracket}})\n    </li>\n  </ul>\n  <hr />\n  <div *ngIf=\"selectedCharacter\">\n  <h2>{{ selectedCharacter.name }} Information</h2>\n  <div><label>Bracket: </label>{{ selectedCharacter.bracket }}</div>\n  <div>\n    <label>Name: </label>\n    <input [(ngModel)]=\"selectedCharacter.name\" placeholder=\"Character Name\" />\n  </div>\n  </div>\n  ",
+        styles: ["\n  .selected {\n    background-color: #CFD8DC !important;\n    color: white;\n  }\n  .characters {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n    width: 15em;\n  }\n  .characters li {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .3em 0;\n    height: 1.6em;\n    border-radius: 4px;\n  }\n  .characters li.selected:hover {\n    background-color: #BBD8DC !important;\n    color: white;\n  }\n  .characters li:hover {\n    color: #607D8B;\n    background-color: #DDD;\n    left: .1em;\n  }\n  .characters .text {\n    position: relative;\n    top: -3px;\n  }\n  .characters .badge {\n    display: inline-block;\n    font-size: small;\n    color: white;\n    padding: 0.8em 0.7em 0 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -4px;\n    height: 1.8em;\n    margin-right: .8em;\n    border-radius: 4px 0 0 4px;\n  }\n"]
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
+var Character = (function () {
+    function Character() {
+    }
+    return Character;
+}());
+exports.Character = Character;
+var Characters = [
+    { id: 1, name: 'Corrin (M)', bracket: 'A' },
+    { id: 2, name: 'Tiki (Y)', bracket: 'A' },
+    { id: 3, name: 'Ninian', bracket: 'A' },
+    { id: 4, name: 'Fae', bracket: 'A' },
+    { id: 5, name: 'Tiki (O)', bracket: 'B' },
+    { id: 6, name: 'Nowi', bracket: 'B' },
+    { id: 7, name: 'Corrin (F)', bracket: 'B' },
+    { id: 8, name: 'Sophia', bracket: 'B' }
+];
 //# sourceMappingURL=app.component.js.map
--- heroes-application/src/app/app.component.ts
+++ heroes-application/src/app/app.component.ts
@@ -1,7 +1,106 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+
+  template: `
+  <h1>Welcome to {{ title }}!</h1>
+  <h2>Gauntlet Character List</h2>
+  <ul class="characters">
+    <li *ngFor="let character of characters" 
+      (click)="onSelect(character)" 
+      [class.selected]="character === selectedCharacter" >
+    <span class="badge"><b>{{character.id}}</b></span> {{character.name}}
+        <label><b>(Bracket: </b></label>
+        {{character.bracket}})
+    </li>
+  </ul>
+  <hr />
+  <div *ngIf="selectedCharacter">
+  <h2>{{ selectedCharacter.name }} Information</h2>
+  <div><label>Bracket: </label>{{ selectedCharacter.bracket }}</div>
+  <div>
+    <label>Name: </label>
+    <input [(ngModel)]="selectedCharacter.name" placeholder="Character Name" />
+  </div>
+  </div>
+  `,
+
+  styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .characters {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .characters li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .characters li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .characters li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .characters .text {
+    position: relative;
+    top: -3px;
+  }
+  .characters .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  { 
+  title = 'Tip The Scales';
+  characters = Characters;
+  selectedCharacter: Character;
+  onSelect(character: Character): void {
+    this.selectedCharacter = character;
+  }
+}
+
+export class Character {
+  id: number;
+  name: string;
+  bracket: string;
+}
+
+const Characters: Character[] = [
+  { id: 1, name: 'Corrin (M)', bracket: 'A' },
+  { id: 2, name: 'Tiki (Y)', bracket: 'A' },
+  { id: 3, name: 'Ninian', bracket: 'A' },
+  { id: 4, name: 'Fae', bracket: 'A' },
+  { id: 5, name: 'Tiki (O)', bracket: 'B' },
+  { id: 6, name: 'Nowi', bracket: 'B' },
+  { id: 7, name: 'Corrin (F)', bracket: 'B' },
+  { id: 8, name: 'Sophia', bracket: 'B' }
+];
